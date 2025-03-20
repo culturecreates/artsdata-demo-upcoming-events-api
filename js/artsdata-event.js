f4fd00dd93c63a055da28ff18ce26dda7cd097dd
Gregory Saumier-Finch <gregory@culturecreates.com>
2025-03-20 10:44:29 -0400

    Pointed to Artsdata.ca

--- js/artsdata-event.js
+++ js/artsdata-event.js
@@ -1,26 +1,47 @@
 class ArtsdataEvent extends HTMLElement {
   set event(event) {
     this.innerHTML = `<div>
-    <a href='${ event.url ||  event.urlFr ||  event.urlEn }'>
-    <h3> ${ event.nameFr }</h3></a>
-    <p> ${ this.attendanceMode(event.eventAttendanceMode) } </p>
-    <div class="box"><img src="${ event.image || '' }"></div>
-    <p> ${ this.dateFormat(event) } </p></div>`
+   
+    <div class="box"><a href="${event.url}"><img src="${ event.image || '' }"></a></div>
+    <h3> ${ event.nameFr|| event.nameEn}  ${ this.eventStatus(event.eventStatus) }</h3>
+    <p> ${ this.dateFormat(event) } 
+    <br>${ this.location(event.location) } 
+     ${ this.attendanceMode(event.attendanceMode) } </p>
+   
+    </div>`
   }
 
+  eventStatus(eventStatus) {
+    if (eventStatus != "EventScheduled")  {
+      return ` - <span style='color: red;'>${eventStatus}</span> ` ;
+    }  else {
+      return ""
+    }
+  }
+
+
   attendanceMode(mode) {
-    var modeHtml = ""
+    var html = ""
     if (mode == "MixedEventAttendanceMode") {
-      modeHtml = "AUSSI EN SPECTACLE VIRTUEL"
+      html = "AUSSI EN SPECTACLE VIRTUEL"
     }
     else if (mode == "OnlineEventAttendanceMode") {
-      modeHtml = "SPECTACLE VIRTUEL"
+      html = "SPECTACLE VIRTUEL"
     }
-    return `<span style='color: red;'>${modeHtml}</span>`
+    return `<span style='color: red;'>${html}</span>`
   }
 
   dateFormat(event) {
-    return new Date(event.startDate).toLocaleString('fr-FR',  { dateStyle: 'long', timeStyle: 'short', hour12: false, timeZone: 'EST' }) || event.startDateWithoutTime
+    var html = ""
+    html +=  new Date(event.startDate).toLocaleString('fr-FR',  { dateStyle: 'long', timeStyle: 'short', hour12: false, timeZone: 'EST' }) 
+    return html
+  }
+
+
+  location(location) {
+    var html = ""
+    html +=`${location.nameFr || location.nameEn || location.namePref}`
+    return html;
   }
 }
 
